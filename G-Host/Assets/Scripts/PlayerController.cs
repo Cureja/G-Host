4b97aa2cb37a85b4c5130cbe0bc565e81b80f806
qaramelon <79352849+qaramelon@users.noreply.github.com>
2021-02-20 06:53:49 -0800

    gramma

--- G-Host/Assets/Scripts/PlayerController.cs
+++ G-Host/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public List<Interactive> interactives = new List<Interactive>();
     public Rigidbody rb;
     public float maxMove;
     public float accMove;
@@ -23,7 +24,13 @@ public class PlayerController : MonoBehaviour
     {
         float vertical = Input.GetAxisRaw("Vertical");
         float horizontal = Input.GetAxisRaw("Horizontal");
+        
+        if(Input.GetKeyDown("space")&&interactives.Count!=0)
+        {
+            interactives[0].Interacted(); 
+        }
 
+        interactives.Clear();
         input = new Vector2(horizontal, vertical);
         Vector3 velocity = rb.velocity;
 
@@ -45,5 +52,13 @@ public class PlayerController : MonoBehaviour
 
         rb.velocity = velocity;
     }
-
+    void onTriggerStay(Collider other)
+    {
+        transform.position = new Vector3(10, 10, 0);
+        if (other.gameObject.layer == LayerMask.NameToLayer("Interactive Npc"))
+        {
+            
+            //interactives.Add(other.gameObject.GetComponent<Interactive>());
+        }
+    }
 }
